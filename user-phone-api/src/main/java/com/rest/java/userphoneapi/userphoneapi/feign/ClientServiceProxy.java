836260ed052263845807388aef31644571682a90
luisorellana777 <luis.orellana777@gmail.com>
2020-09-05 13:13:01 -0400

    addition of ribbon

--- user-phone-api/src/main/java/com/rest/java/userphoneapi/userphoneapi/feign/ClientServiceProxy.java
+++ user-phone-api/src/main/java/com/rest/java/userphoneapi/userphoneapi/feign/ClientServiceProxy.java
@@ -2,12 +2,14 @@ package com.rest.java.userphoneapi.userphoneapi.feign;
 
 import java.util.Collection;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.rest.java.userphoneapi.userphoneapi.dto.Client;
 
+@RibbonClient(name="client-api")
 @FeignClient(name = "client-api")
 public interface ClientServiceProxy {
 	
